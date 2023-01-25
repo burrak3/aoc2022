d2d21ff558b4f70295682faa62ff419bbd95cbd4
Lalit Kumar <lalit.kumar@hypersonix.ai>
2023-01-25 23:37:54 +0530

    feat(day1): types and error handling using crates

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,27 +1,13 @@
-fn main() {
-    let input = read_input().unwrap();
+use color_eyre::eyre::Context;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let input = read_input()?;
     println!("{}", input);
+    Ok(())
 }
 
-impl std::fmt::Debug for PathedIoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "for file {:?}: {}", self.path, self.inner)
-    }
-}
-
-/// An [std::io::Error] associated with a path
-struct PathedIoError {
-    path: String,
-    inner: std::io::Error,
-}
-
-fn read_input() -> Result<String, PathedIoError> {
-    let path = "src/input.txt";
-    match std::fs::read_to_string(path) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(PathedIoError {
-            path: path.into(),
-            inner: e,
-        }),
-    }
+fn read_input() -> color_eyre::Result<String> {
+    let input = std::fs::read_to_string("src/input.txt").wrap_err("reading src/input.txt")?;
+    Ok(input)
 }
